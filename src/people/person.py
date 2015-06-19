6c6c6b532f8932f1a69bcf7451bcbd9f27f870d9
Srav <sravanthi.nalamalapu@gmail.com>
2015-06-18 22:18:20 -0400

    Commit 1

---
+++ src/people/person.py
@@ -0,0 +1,38 @@
+__author__ = 'snalam200'
+
+
+class Person(object):
+    def __init__(self, name, address=None, phone_num=None):
+        self.name = name
+        self.address = address
+        self.phone_num = phone_num
+
+
+class Employee(Person):
+    def __init__(self, name, employee_num, designation, hourly_pay, address=None, phone_num=None):
+        super(Employee, self).__init__(name, address, phone_num)
+        self.employee_num = employee_num
+        self.designation = designation
+        self.hourly_pay = float(hourly_pay)
+
+
+class Manager(Employee):
+    def __init__(self, name, employee_num, designation, hourly_pay, address=None, phone_num=None):
+        super(Manager, self).__init__(name, employee_num, designation, hourly_pay, address, phone_num)
+    def funs(self):
+        pass
+
+
+class Cashier(Employee):
+    def __init__(self, name, employee_num, designation, hourly_pay, address=None, phone_num=None):
+        super(Cashier, self).__init__(name, employee_num, designation, hourly_pay, address, phone_num)
+    def funs(self):
+        pass
+
+
+class Customer(Person):
+    def __init__(self, name, address=None, phone_num=None):
+        super(Customer, self).__init__(name, address, phone_num)
+
+
+
